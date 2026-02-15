b5076c2598dcb258e0ec858e08d1937445609ca7
deyperfect <edrylpalinis@gmail.com>
2026-02-15 11:41:35 +0800

    fix: force rebuild without basename

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import './style.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Wanted-Minds">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
