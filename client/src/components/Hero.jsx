52f6f33f224fbdbff865380877e8eda5b8c65056
deyperfect <edrylpalinis@gmail.com>
2026-03-16 20:48:30 +0800

    feat: add Hero component

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,19 @@
+const Hero = ({tagline = 'The minds you want when answers are hard to find.'}) => {
+    return (
+        <section id="hero" className="row">
+        <img 
+          src="/images/backdrop.webp" 
+          alt="Hero background" 
+          fetchPriority="high"
+          className="hero-background-image"
+        />
+        <div className="col-9 col-md-7 mx-auto my-auto text-center">
+          <div id="hero-text" className="my-auto">
+            <h1>{tagline}</h1>
+          </div>
+        </div>
+      </section>
+    )
+}
+
+export default Hero;
