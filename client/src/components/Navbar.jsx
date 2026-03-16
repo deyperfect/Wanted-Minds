bff2dc2c76aa34d5ab16ee6b3a3437a764aec0da
deyperfect <edrylpalinis@gmail.com>
2026-03-16 20:48:54 +0800

    feat: add Navbar component

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,52 @@
+import { Link, NavLink } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Menu } from "lucide-react";
+
+const NavigationBar = () => {
+  return (
+    <Navbar expand="lg" sticky="top">
+      <Container fluid className="container-lg">
+        {/* 'as={Link}' allows the brand to act as a Router Link */}
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src={`${import.meta.env.BASE_URL}images/logo.webp`}
+            className="brand-logo d-inline-block align-top"
+            alt="Wanted Minds Logo"
+          />
+        </Navbar.Brand>
+
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          className="custom-toggler"
+        >
+          <span>
+            <Menu className="list" />
+          </span>
+        </Navbar.Toggle>
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto text-center">
+            {/* NavLink automatically adds an "active" class to the current page */}
+            <Nav.Link as={NavLink} to="/" end>
+              Home
+            </Nav.Link>
+
+            <Nav.Link as={NavLink} to="/team" end>
+              Our Team
+            </Nav.Link>
+
+            <Nav.Link href="/#contact">
+              Contact Us
+            </Nav.Link>
+
+          </Nav>
+          <Link to="/contact" className="btn btn-outline-light ms-lg-3">
+            File a Case
+          </Link>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
