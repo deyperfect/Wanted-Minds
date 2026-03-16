9fb82e6c3eb5eca18d92ec6cda2d8916eda909eb
deyperfect <edrylpalinis@gmail.com>
2026-03-16 20:50:27 +0800

    feat: add Team component

---
+++ client/src/components/Team.jsx
@@ -0,0 +1,24 @@
+import TeamCard from "./TeamCard";
+import team from '../data/team';
+
+const Team = ({ sectionTitle = "Behind Closed Doors" }) => {
+  return (
+    <section id="team" className="py-5">
+      <h2 className="text-center mb-5"> {sectionTitle}</h2>
+
+      <div className="container2">
+        <div className="row justify-content-center g-5">
+
+            {team.map((detective) => (
+              <div key={detective.id} className="col-10 col-md-6 col-lg-4 detectives">
+                <TeamCard detective={detective} />
+              </div>
+            ))}
+
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Team;
