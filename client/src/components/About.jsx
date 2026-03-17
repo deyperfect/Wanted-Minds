c4834aa8d05810afb7c20fe0a726619a4c5afbba
deyperfect <edrylpalinis@gmail.com>
2026-03-17 22:33:11 +0800

    update: About section layout and styling

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -5,11 +5,14 @@ const About = ({
   paragraphs = about.paragraphs,
 }) => {
   return (
-    <section id="about">
+    <section id="about" className='container'>
       <div id="intro">
-        <h2>{sectionTitle}</h2>
         <div className="row">
-          <div className="col-10 mx-auto">
+          <div id="about-image" className="col-10 col-lg-6">
+            <img src="/images/location.webp" alt="Image outside Wanted Minds headquarters" />
+          </div>
+          <div className="col-10 col-lg-6">
+            <h2>{sectionTitle}</h2>
             {paragraphs.map((paragraph, index) => (
               <p key={index}>{paragraph}</p>
             ))}
