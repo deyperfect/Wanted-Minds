7885dbc1e304570be27716fc60ba4310945c7146
deyperfect <edrylpalinis@gmail.com>
2026-03-17 22:38:01 +0800

    refactor: update Contact to use new ContactForm component

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,13 +1,4 @@
-const fields = [
-  { label: "Name", type: "text", placeholder: "Your Name" },
-  {
-    label: "Email",
-    type: "email",
-    placeholder: "email@example.com",
-    required: true,
-  },
-  { label: "Message", as: "textarea", rows: 4, required: true },
-];
+import ContactForm from "./ContactForm";
 
 const Contact = () => {
   return (
@@ -17,34 +8,9 @@ const Contact = () => {
         className="row justify-content-between justify-content-lg-between g-4"
         id="contact-content"
       >
-        <div
-          className="col-10 col-md-8 col-lg-6 mx-auto"
-          id="address-holder"
-        >
-          <div className="address-content">
-            <p className="address-label mb-2">HEADQUARTERS</p>
+        
+        <ContactForm />
 
-            <address className="mb-0">
-              4-12-7 Aoyama-Cyber <br />
-              Minato-ku <br />
-              Tokyo 107-0062
-            </address>
-          </div>
-        </div>
-
-        <div className="col-10 col-lg-6 mx-auto" id="contact-form">
-          <form>
-            {fields.map(({ label, as: Tag = "input", ...props }) => (
-              <div key={label} className="mb-3">
-                <label className="form-label">{label}</label>
-                <Tag className="form-control" {...props} />
-              </div>
-            ))}
-            <button className="btn btn-outline-light" type="submit">
-              Send Message
-            </button>
-          </form>
-        </div>
       </div>
     </section>
   );
