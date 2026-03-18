f3c5c8c247327a18b536dcf2cf6c0169f606d868
deyperfect <edrylpalinis@gmail.com>
2026-03-18 21:58:17 +0800

    update: Hero section layout and styling

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,15 +1,23 @@
+import HeroButtons from "./HeroButtons";
+
+
 const Hero = ({tagline = 'The minds you want when answers are hard to find.'}) => {
     return (
-        <section id="hero">
+      <section id="hero">
         <img 
           src="/images/backdrop.webp" 
           alt="Hero background" 
           fetchPriority="high"
           className="hero-background-image"
         />
-        <div className="col-9 col-md-7 mx-auto my-auto text-center">
-          <div id="hero-text" className="my-auto">
-            <h1>{tagline}</h1>
+        <div className="row justify-content-center">
+          <div className="col-9 col-md-7">
+            <div id="hero-text">
+              <h1>{tagline}</h1>
+            </div>
+          </div>
+          <div className="col-9 col-md-7 text-center mt-5">
+            <HeroButtons />
           </div>
         </div>
       </section>
