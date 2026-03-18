f3571a1498f185f35caa335eb4c77288f917e585
deyperfect <edrylpalinis@gmail.com>
2026-03-18 21:59:32 +0800

    update: About section layout, styling, and responsiveness

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,4 +1,5 @@
-import about from '../data/about.json'
+import about from '../data/about.json';
+import { Link } from 'react-router-dom';
 
 const About = ({
   sectionTitle = about.sectionTitle,
@@ -6,17 +7,23 @@ const About = ({
 }) => {
   return (
     <section id="about" className='container'>
-      <div id="intro">
-        <div className="row">
-          <div id="about-image" className="col-10 col-lg-6">
-            <img src="/images/location.webp" alt="Image outside Wanted Minds headquarters" />
+      <div id="intro" >
+        <div className="row justify-content-center">
+          <div id="about-image" className="col-11 col-lg-6">
+            <div className="about-image-inner">
+              <img src="/images/location.webp" alt="Image outside Wanted Minds headquarters" className="img-fluid" />
+            </div>
           </div>
-          <div className="col-10 col-lg-6">
-            <h2>{sectionTitle}</h2>
-            {paragraphs.map((paragraph, index) => (
-              <p key={index}>{paragraph}</p>
-            ))}
+          <div id='about-text' className="col-11 col-lg-6" >
+            <div>
+              <h2>{sectionTitle}</h2>
+              {paragraphs.map((paragraph, index) => (
+                <p key={index}>{paragraph}</p>
+              ))}
+            </div>
+            <Link to="/about" className='btn btn-dark'> Learn More </Link>
           </div>
+          
         </div>
       </div>
     </section>
