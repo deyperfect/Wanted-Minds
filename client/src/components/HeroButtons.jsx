df57e94b9c0c4ca0ee364bfd40f7397218a8c125
deyperfect <edrylpalinis@gmail.com>
2026-03-19 20:06:33 +0800

    fix: resolve #contact anchor link in HeroButtons component

--- client/src/components/HeroButtons.jsx
+++ client/src/components/HeroButtons.jsx
@@ -4,9 +4,9 @@ const HeroButtons = () => {
   return (
     <>
       <div className="d-flex justify-content-center" id="hero-btn">
-        <Link to="/contact" className="btn btn-lg" id="hero-contact">
+        <a href="/#contact" className="btn btn-lg" id="hero-contact">
           Contact
-        </Link>
+        </a>
 
         <Link to="/contact" className="btn btn-outline-light btn-lg" id="hero-file">
           Open a Case
