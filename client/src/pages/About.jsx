29f43b09273c1d398671ef73737c3eb091dd79ca
deyperfect <edrylpalinis@gmail.com>
2026-03-19 20:08:18 +0800

    feat: add about page component

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,15 @@
+import Inspiration from '../components/Inspiration';
+import Testimonials from '../components/Testimonials';
+import Team from '../components/Team'
+
+const About = () => {
+  return (
+    <>
+      <Inspiration />
+      <Team />
+      <Testimonials />
+    </>
+  )
+}
+
+export default About;
