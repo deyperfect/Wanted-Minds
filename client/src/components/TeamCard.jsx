ba4f664ba1623bc7e4e089b1cc3c7ac4fcdfe66c
deyperfect <edrylpalinis@gmail.com>
2026-03-19 20:03:53 +0800

    feat: add TeamCard component

--- client/src/components/TeamCard.jsx
+++ client/src/components/TeamCard.jsx
@@ -2,15 +2,18 @@
 
 const TeamCard = ({ detective }) => {
   return (
-    <div className="d-flex flex-column h-100" id="card1">
-      <img
-        className="icon img-fluid"
-        id="img1"
-        src={detective.image}
-        alt={detective.alt}
-      />
-      <p className="eng-name">{detective.engName}</p>
-      <p className="jp-name">{detective.jpName}</p>
+    <div className="d-flex flex-column h-100" id="team-card">
+      <div className="image-holder">
+        <img
+          className="img-fluid"
+          src={detective.image}
+          alt={detective.alt}
+        />
+      </div>
+      <div id="detective-details">
+        <p className="eng-name">{detective.engName}</p>
+        <p className="alias">{detective.alias}</p>
+      </div>
       <p className="desc">
         {detective.info}
       </p>
